556c0f7fe044dd8353db48420bf68e1a270241ad
Tommy Coleman <tommiilee@gmail.com>
2020-02-19 18:38:09 -0500

    solved rock paper scissors

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,27 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches_per_ingredient = []
+    for ingredient in recipe.keys():
+        if ingredient in ingredients:
+            batches_per_ingredient.append(
+                ingredients[ingredient] // recipe[ingredient])
+        else:
+            return 0
+
+    max_batch = batches_per_ingredient[0]
+    for batch in batches_per_ingredient:
+        if batch < max_batch:
+            max_batch = batch
+    return max_batch
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,40 @@
 
 import sys
 
+
+def rps_helper(args):
+    plays = []
+    for play in args:
+        rock = list(play)
+        rock.append('rock')
+        paper = list(play)
+        paper.append('paper')
+        scissors = list(play)
+        scissors.append('scissors')
+        plays.append(rock)
+        plays.append(paper)
+        plays.append(scissors)
+
+    return plays
+
+
 def rock_paper_scissors(n):
-  pass 
+    plays = [['rock'], ['paper'], ['scissors']]
+    if n == 0:
+        return [[]]
+    while n > 1:
+        n = n - 1
+        plays = rps_helper(plays)
+        rock_paper_scissors(n)
+
+    return plays
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
+
+print(rps_helper([['rock'], ['paper'], ['scissors']]))
